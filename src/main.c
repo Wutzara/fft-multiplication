3b7544e4c19fad69ac7808ebd5de0a0d456f6f85
Günther Wutz <info@gunibert.de>
2015-06-14 17:51:06 +0200

    formatted source

--- src/main.c
+++ src/main.c
@@ -17,47 +17,47 @@
 
 typedef struct {
   int n;
-  mycomplex* vec1;
-  mycomplex* vec2;
+  mycomplex *vec1;
+  mycomplex *vec2;
 } inputdata;
 
-inputdata* open_file(char *filename) {
+inputdata *open_file(char *filename) {
   FILE *fp = fopen(filename, "r");
   int i;
-  
-  if(fp == NULL) {
+
+  if (fp == NULL) {
     fprintf(stderr, "%s\n", "Error while opening the file");
     exit(EXIT_FAILURE);
   }
 
   char line[128];
   inputdata *data = malloc(sizeof(inputdata));
-  for(i = 0; fgets(line, sizeof(line), fp) != NULL; i++) {
-    switch(i) {
+  for (i = 0; fgets(line, sizeof(line), fp) != NULL; i++) {
+    switch (i) {
     case 0: {
-              data->n = atoi(line);
-            break;
-            }
+      data->n = atoi(line);
+      break;
+    }
     case 1: {
-              int j;
-              data->vec1 = malloc(sizeof(mycomplex)*data->n);
-              char *token = strtok(line, " ");
-              for(j = 0; token; j++, token = strtok(NULL, " ")) {
-                data->vec1[j].real = atof(token);
-                data->vec1[j].imag = 0.;
-              }
-            break;
-            }
+      int j;
+      data->vec1 = malloc(sizeof(mycomplex) * data->n);
+      char *token = strtok(line, " ");
+      for (j = 0; token; j++, token = strtok(NULL, " ")) {
+        data->vec1[j].real = atof(token);
+        data->vec1[j].imag = 0.;
+      }
+      break;
+    }
     case 2: {
-              int j;
-              data->vec2 = malloc(sizeof(mycomplex)*data->n);
-              char *token = strtok(line, " ");
-              for(j = 0; token; j++, token = strtok(NULL, " ")) {
-                data->vec2[j].real = atof(token);
-                data->vec2[j].imag = 0.;
-              }
-            break;
-            }
+      int j;
+      data->vec2 = malloc(sizeof(mycomplex) * data->n);
+      char *token = strtok(line, " ");
+      for (j = 0; token; j++, token = strtok(NULL, " ")) {
+        data->vec2[j].real = atof(token);
+        data->vec2[j].imag = 0.;
+      }
+      break;
+    }
     }
   }
 
@@ -67,18 +67,18 @@ inputdata* open_file(char *filename) {
 
 int main(int argc, char *argv[]) {
   int i;
-  if(argc != 2) {
+  if (argc != 2) {
     printf("Usage: %s inputdata\n", argv[0]);
     return EXIT_FAILURE;
   }
 
-  inputdata* data = open_file(argv[1]);
+  inputdata *data = open_file(argv[1]);
   mycomplex *ret = malloc(sizeof(mycomplex) * data->n);
   mycomplex *ret2 = malloc(sizeof(mycomplex) * data->n);
   mycomplex *interm = malloc(sizeof(mycomplex) * data->n);
   recursive_fft(data->vec1, ret, data->n, 0);
   recursive_fft(data->vec2, ret2, data->n, 0);
-  
+
   /* multiplikation der 2 vektoren */
   for (i = 0; i < data->n; ++i) {
     interm[i] = mul(ret[i], ret2[i]);
@@ -91,7 +91,7 @@ int main(int argc, char *argv[]) {
   for (i = 0; i < data->n; ++i) {
     ret[i].real /= data->n;
   }
-  
+
   print_vector(ret, data->n);
 
   free(data->vec1);
