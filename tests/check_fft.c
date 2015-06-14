b09640d821ed7b746193ddfcdb2ff001bc2ccb5b
Günther Wutz <info@gunibert.de>
2015-06-14 15:55:57 +0200

    switched to library

--- tests/check_fft.c
+++ tests/check_fft.c
@@ -1,7 +1,7 @@
 
 #include <check.h>
 #include <stdlib.h>
-#include "../src/fft.h"
+#include <fft/fft.h>
 
 START_TEST(test_fft_recursive) {
   int n = 4;
@@ -19,16 +19,35 @@ START_TEST(test_fft_recursive) {
   }
   recursive_fft(vec, y, n, 0);
   for (i = 0; i < n; ++i) {
-    ck_assert_msg(y[i].real == y_erg_real[i], 
-        "expected: %f - found: %f", y[i].real, y_erg_real[i]);
-    ck_assert_msg(y[i].imag == y_erg_imag[i], 
-        "expected: %f - found: %f", y[i].imag, y_erg_imag[i]);
+    ck_assert_msg(y[i].real == y_erg_real[i], "expected: %f - found: %f",
+                  y[i].real, y_erg_real[i]);
+    ck_assert_msg(y[i].imag == y_erg_imag[i], "expected: %f - found: %f",
+                  y[i].imag, y_erg_imag[i]);
   }
   free(vec);
   free(y);
 }
 END_TEST
 
+START_TEST(test_complex_add) {
+  mycomplex a, b, c;
+  a.real = 10; a.imag = 20;
+  b.real = 10; b.imag = 20;
+  c = add(a,b);
+  ck_assert_msg(c.real == 20., "expected: %f - found: %f", 20., c.real);
+  ck_assert_msg(c.imag == 40., "expected: %f - found: %f", 40., c.imag);
+}
+END_TEST
+
+START_TEST(test_complex_sub) {
+  mycomplex a, b, c;
+  a.real = 10; a.imag = 20;
+  b.real = 5; b.imag = 10;
+  c = sub(a,b);
+  ck_assert_msg(c.real == 5., "expected: %f - found: %f", 5., c.real);
+  ck_assert_msg(c.imag == 10., "expected: %f - found: %f", 10., c.imag);
+}
+END_TEST
 Suite *fft_suite(void) {
   Suite *s;
   TCase *tc_core;
@@ -37,6 +56,8 @@ Suite *fft_suite(void) {
   tc_core = tcase_create("Core");
 
   tcase_add_test(tc_core, test_fft_recursive);
+  tcase_add_test(tc_core, test_complex_add);
+  tcase_add_test(tc_core, test_complex_sub);
   suite_add_tcase(s, tc_core);
 
   return s;
